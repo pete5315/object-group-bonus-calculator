b9c3d6a3ec088b2c616abd41bfde2f07f8b6e1df
pete5315 <124735535+pete5315@users.noreply.github.com>
2023-05-02 14:06:22 -0500

    Initial commit: working for employee rating 1-2

--- client.js
+++ client.js
@@ -44,14 +44,45 @@ console.log('array of employee data: ',  employees );
 // Ask questions when you don't.
 
 
-
-
 // This function will calculate 1 employee's bonus!
 //
 function calculateIndividualEmployeeBonus( employee ) {  
   // your logic here
-  
-  
-  // return new object with bonus results
+  let object = {
+    name: 'Mayella',
+    bonusPercentage: '89068',
+    totalCompensation: '35000',
+    totalBonus: 1
+  };
+  object.name=employee.name;
+// if conditionals regarding rating 
+  if (employee.reviewRating <= 2) {
+    object.bonusPercentage = 0;
+    object.totalCompensation = employee.annualSalary + object.bonusPercentage;
+    object.totalBonus = object.totalCompensation - employee.annualSalary;
+  }
 
+  if (employee.reviewRating === 3) {
+    object.bonusPercentage = 0;
+    object.totalCompensation = employee.annualSalary + object.bonusPercentage;
+    object.totalBonus = object.totalCompensation - employee.annualSalary;
+  }
+// establishes base bonus 
+
+
+// employee number modification
+
+
+// high income modification
+
+
+// floor or ceiling
+
+  console.log(object);
+  // return new object with bonus results
+  return object
+}
+
+for (let person of employees) {
+  calculateIndividualEmployeeBonus(person);
 }
